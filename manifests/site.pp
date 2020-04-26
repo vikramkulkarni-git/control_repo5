355709438dae39aa3878beb85da776fdb3c347c9
vikramkulkarni-git <63256800+vikramkulkarni-git@users.noreply.github.com>
2020-04-26 23:08:48 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,13 @@
 node default {
-  file { '/root/README':
-    ensure => file,
-    content => 'This is a readme',
-    owner   => 'root',
-  }
-}
+    }
+
 node 'master.puppet.vm' {
   include role::master_server
-}
+   file { '/root/README':
+    ensure => file,
+    content => $fqdn,
+       }
+ }
 node /^web/ { 
   include role::app_server
 }
